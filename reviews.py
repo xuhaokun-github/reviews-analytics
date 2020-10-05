caa422a1fa556e57b2fea40a7a9285616fbc8663
xuhaokun <kingxuhaokun@gmail>
2020-10-05 23:06:57 +0800

    End? no

--- reviews.py
+++ reviews.py
@@ -6,9 +6,18 @@ with open('reviews (1).txt','r') as f:
 		count += 1	
 		if count % 1000 == 0:
 			print(len(data))
+
 print('档案读取完了,总共有', len(data), '笔留言')
 
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言的平均长度是', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '笔留言长度小于100')
+print(new[0])
+print(new[1])
